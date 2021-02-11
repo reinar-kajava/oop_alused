0305f3ac4127ee71356e3b29b2b5f7de964531b6
reinar-kajava <74591327+reinar-kajava@users.noreply.github.com>
2021-02-11 22:35:35 +0000

    Delete Restoran.py

--- Restoran.py
+++
@@ -1,9 +0,0 @@
-class Restoran():
-    restorani_nimi = ""
-    soogi_tyyp = ""
-    #atribuudid
-    def restorani_kirjeldus(self):
-        print("Restoran" + str(self.restorani_nimi) + ", söögi tüüp" + str(self.soogi_tyyp))
-
-    def ava_restoran(self):
-        print("Restoran on avatud")
