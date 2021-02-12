16528f50024611715bb3305a34e6d61cd78acaab
reinar-kajava <74591327+reinar-kajava@users.noreply.github.com>
2021-02-12 08:45:20 +0000

    Kasutajanimede ülesande lõplik lahendus

---
+++ 2. teema/kasutajafail.py
@@ -0,0 +1,25 @@
+from Kasutajad import Kasutajad
+#Kasutaja 1
+kasutaja1 = Kasutajad()
+kasutaja1.eesnimi = "Joonas"
+kasutaja1.perenimi = "Tamm"
+kasutaja1.kasutajanim = "Tammus"
+kasutaja1.parool = "123461235"
+kasutaja1.kasutaja_kirjeldus()
+kasutaja1.kasutaja_tervitus()
+
+kasutaja2 = Kasutajad()
+kasutaja2.eesnimi = "Bob"
+kasutaja2.perenimi = "Lancaster"
+kasutaja2.kasutajanim = "Bob.Lancaster41"
+kasutaja2.parool = "Lanny436"
+kasutaja2.kasutaja_kirjeldus()
+kasutaja2.kasutaja_tervitus()
+
+kasutaja3 = Kasutajad()
+kasutaja3.eesnimi = "Alice"
+kasutaja3.perenimi = "Ime"
+kasutaja3.kasutajanim = "Alice_Imedemaal"
+kasutaja3.parool = "Wo0NderlaNd12350000"
+kasutaja3.kasutaja_kirjeldus()
+kasutaja3.kasutaja_tervitus()
