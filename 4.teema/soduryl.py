a3be4dde78094e1b092517a73d49bd9084504ca2
reinar-kajava <74591327+reinar-kajava@users.noreply.github.com>
2021-02-18 11:56:59 +0000

    Sõduri ÜL lõplik lahendus.

--- 4.teema/soduryl.py
+++ 4.teema/soduryl.py
@@ -5,17 +5,18 @@ armee2 = []
 armee3 = []
 
 for kord in range(1, 21, 1):
-    armeenr = randint(1, 2)
-if (armeenr == 1):
-    sodur = Sodur(1)
-    armee1.append(sodur)
-if (armeenr == 2):
-    sodur = Sodur(2)
-    armee1.append(sodur)
-print(armee1)
+    armee_nr = randint(1, 2)
+    if(armee_nr == 1):
+        sodur = Sodur(1)
+        armee1.append(sodur)
+    if (armee_nr == 2):
+        sodur = Sodur(2)
+        armee2.append(sodur)
+# väljastame armeede sisu
+print("Armee 1")
 for sodur in armee1:
     sodur.info()
-print("-----------------------------")
-print(armee2)
+print("-------------------------")
+print("Armee 2")
 for sodur in armee2:
     sodur.info()
